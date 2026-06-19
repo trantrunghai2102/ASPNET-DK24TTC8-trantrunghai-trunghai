b107255dc3fad1ed040799344ffe442cdfe0106c
trantrunghai2102 <kiemtradda@gmail.com>
2026-06-19 13:03:49 +0700

    fix: display vnd price

---
+++ src/BanCaPheNuocGiaiKhat/Data/Migrations/20260619055753_UpdateProductPrices.cs
@@ -0,0 +1,564 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanCaPheNuocGiaiKhat.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductPrices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 1,
+                column: "base_price",
+                value: 220000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 2,
+                column: "base_price",
+                value: 190000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 3,
+                column: "base_price",
+                value: 210000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 4,
+                column: "base_price",
+                value: 240000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 5,
+                column: "base_price",
+                value: 200000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 6,
+                column: "base_price",
+                value: 230000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 7,
+                column: "base_price",
+                value: 180000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 8,
+                columns: new[] { "base_price", "detail_desc" },
+                values: new object[] { 200000m, "Tarrazu region provides one of the finest beans in Central America." });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 9,
+                column: "base_price",
+                value: 210000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 10,
+                column: "base_price",
+                value: 190000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 11,
+                column: "base_price",
+                value: 190000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 12,
+                column: "base_price",
+                value: 250000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 13,
+                column: "base_price",
+                value: 450000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 14,
+                column: "base_price",
+                value: 390000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 15,
+                column: "base_price",
+                value: 220000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 16,
+                column: "base_price",
+                value: 160000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 17,
+                column: "base_price",
+                value: 190000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 18,
+                column: "base_price",
+                value: 170000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 19,
+                column: "base_price",
+                value: 350000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 20,
+                column: "base_price",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 21,
+                column: "base_price",
+                value: 550000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 22,
+                column: "base_price",
+                value: 400000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 23,
+                column: "base_price",
+                value: 650000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 24,
+                column: "base_price",
+                value: 550000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 25,
+                column: "base_price",
+                value: 240000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 26,
+                column: "base_price",
+                value: 210000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 27,
+                column: "base_price",
+                value: 220000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 28,
+                column: "base_price",
+                value: 350000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 29,
+                column: "base_price",
+                value: 850000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 30,
+                column: "base_price",
+                value: 200000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 31,
+                column: "base_price",
+                value: 160000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 32,
+                column: "base_price",
+                value: 170000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 33,
+                column: "base_price",
+                value: 170000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 34,
+                column: "base_price",
+                value: 190000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 35,
+                column: "base_price",
+                value: 280000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 36,
+                column: "base_price",
+                value: 220000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 37,
+                column: "base_price",
+                value: 180000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 38,
+                column: "base_price",
+                value: 150000m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 39,
+                column: "base_price",
+                value: 250000m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 1,
+                column: "base_price",
+                value: 22.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 2,
+                column: "base_price",
+                value: 19.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 3,
+                column: "base_price",
+                value: 21.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 4,
+                column: "base_price",
+                value: 24.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 5,
+                column: "base_price",
+                value: 20.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 6,
+                column: "base_price",
+                value: 23.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 7,
+                column: "base_price",
+                value: 18.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 8,
+                columns: new[] { "base_price", "detail_desc" },
+                values: new object[] { 20.00m, null });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 9,
+                column: "base_price",
+                value: 21.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 10,
+                column: "base_price",
+                value: 19.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 11,
+                column: "base_price",
+                value: 19.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 12,
+                column: "base_price",
+                value: 25.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 13,
+                column: "base_price",
+                value: 45.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 14,
+                column: "base_price",
+                value: 39.99m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 15,
+                column: "base_price",
+                value: 22.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 16,
+                column: "base_price",
+                value: 16.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 17,
+                column: "base_price",
+                value: 19.90m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 18,
+                column: "base_price",
+                value: 17.50m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 19,
+                column: "base_price",
+                value: 35.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 20,
+                column: "base_price",
+                value: 30.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 21,
+                column: "base_price",
+                value: 55.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 22,
+                column: "base_price",
+                value: 40.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 23,
+                column: "base_price",
+                value: 65.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 24,
+                column: "base_price",
+                value: 55.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 25,
+                column: "base_price",
+                value: 24.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 26,
+                column: "base_price",
+                value: 21.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 27,
+                column: "base_price",
+                value: 22.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 28,
+                column: "base_price",
+                value: 35.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 29,
+                column: "base_price",
+                value: 85.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 30,
+                column: "base_price",
+                value: 20.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 31,
+                column: "base_price",
+                value: 16.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 32,
+                column: "base_price",
+                value: 17.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 33,
+                column: "base_price",
+                value: 17.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 34,
+                column: "base_price",
+                value: 19.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 35,
+                column: "base_price",
+                value: 28.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 36,
+                column: "base_price",
+                value: 22.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 37,
+                column: "base_price",
+                value: 18.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 38,
+                column: "base_price",
+                value: 15.00m);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "product_id",
+                keyValue: 39,
+                column: "base_price",
+                value: 25.00m);
+        }
+    }
+}
